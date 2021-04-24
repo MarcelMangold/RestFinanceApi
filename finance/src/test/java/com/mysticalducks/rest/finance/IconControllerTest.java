2aa1982c225dae5c82f28d518819b6b61b63d693
MarcelMangold <marcel.gold@outlook.com>
2021-04-24 08:57:08 +0200

    add custom error page

--- finance/src/test/java/com/mysticalducks/rest/finance/IconControllerTest.java
+++ finance/src/test/java/com/mysticalducks/rest/finance/IconControllerTest.java
@@ -6,25 +6,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.security.access.SecurityConfig;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.mysticalducks.rest.finance.controller.IconController;
@@ -46,12 +34,17 @@ public class IconControllerTest {
 
 
 	@Test
-	public void greetingShouldReturnMessage() throws Exception {
+	public void getRequest() throws Exception {
 		given(this.iconService.findIcon(1)).willReturn(new Icon("test"));
 		this.mvc.perform(get("/icon/1").secure(false)).andExpect(status().isOk())
 				.andExpect(content().contentType("application/json"))
 				.andExpect(jsonPath("id").value(0))
 				.andExpect(jsonPath("name").value("test"));
+		
+		this.mvc.perform(get("/icon/").secure(false)).andExpect(status().isOk())
+		.andExpect(content().contentType("application/json"))
+		.andExpect(jsonPath("id").value(0))
+		.andExpect(jsonPath("name").value("test"));
 	}
 
 }
