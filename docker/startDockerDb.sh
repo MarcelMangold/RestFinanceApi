15647ce030f72aaf0122e9c371b4235a2ea4f7a9
MarcelMangold <marcel.gold@outlook.com>
2025-04-18 15:02:09 +0200

    update database version

--- docker/startDockerDb.sh
+++ docker/startDockerDb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm --name finance_db -e POSTGRES_DB=finance_db -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres
+docker run --rm --name finance_db -e POSTGRES_DB=finance_db -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres:17.4
 
